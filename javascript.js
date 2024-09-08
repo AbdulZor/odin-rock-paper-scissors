9e01b02bd06e2a7917314dd24c4a73c37d9673d2
Abdul Zor <57222786+AbdulZor@users.noreply.github.com>
2024-09-08 10:08:13 +0300

    Add function to randomly pick of game choices
    Math.random is used to give a number between 0
    and 1. This random number is used to choose one of
    the game choices. Each game choice is given 1/3
    room to be selected.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // randomly return a string from the following values: rock, paper or scissors.
+
+    // 1. Plan:
+    // Use Math.random to get a number that is random.
+    // IF random number < (1/3)
+    //  THEN RETURN rock
+    // ELSE IF random number > (1/3) AND random number < (2/3)
+    //  THEN RETURN paper
+    // ELSE random number will be bigger than 2/3, but also less than 1
+    //  THEN RETURN scissors
+
+    const randomNumber = Math.random();
+    if (randomNumber < (1 / 3)) {
+        return "rock";
+    } else if (randomNumber >= (1 / 3) && randomNumber < (2 / 3)) {
+        return "paper";
+    }
+
+    return "scissors";
+}
